ea74f197cf8851179ea27852131c358aac361002
slawek mikula <slawek.mikula@gmail.com>
2009-10-03 20:25:42 +0200

    - poprawki w readme i skryptach - usuniecie michala z listy autorow (na razie C+ nie dziala)

--- src/bin/get_listing.sh
+++ src/bin/get_listing.sh
@@ -1,24 +1,17 @@
 #!/bin/sh
-# pobiera listing programow z calego tygodnia od dnia dzisiejszego
-# z kanalow o numerach 1,2,5,17,18,233
-# tj. TVP1, TVP2, POLSAT, TVN, TVP3 Regionalna, TV4
-# 1,2,5,17,18,233
 #
 # pobranie listy kanalow i odpowiadajacym im numerom mozna
 # poprzez opcje -l
 #
-# wiecej w epguide --help
+epguide -l
 
-# uzycie pliku konfiguracji, wyjscie do pliku tv.xmltv
-#
-#  epg_runner --config 'input.conf' --output tv.xmltv
-#
-# backend Canal+, kanaly zgodne z lista, dla calego tygodnia (od dzisiaj)
+# backend Wirtualna Polska, kanaly zgodne z lista, dla calego tygodnia (od dzisiaj)
 # format xmltv, wyjscie do pliku tv.xmltv
 #
-# epg_runner -b cp -c '1,2,5,17,18,233' -w -f xmltv -o tv.xmltv
+epguide -p wp -c 1,2,5,17,18,233 -w -f xmltv -o tv.xmltv
+
 #
 # backend Wp (domyslny), kanaly zgodne z lista, dla jednego dnia, format xmltv,
 # wyjscie do pliku tv.xmltv
 #
-# epg_runner -c '1,2,3' -d '2007-08-12' -f xmltv -o tv.xmltv
+epguide -c 1,2,3 -d 2009-12-24 -f xmltv -o tv.xmltv
