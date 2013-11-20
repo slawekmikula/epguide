c7a8477cec4c830f83a1bcb3d3983f09569d2ca8
Damian Go?da <Damian.Golda@gmail.com>
2013-11-20 14:04:53 +0100

    Wersja 1.8.0

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=1.8.0-Alpha2
+VERSION=1.8.0
 PKG_NAME=epguide
 
 egg:
--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup (
     name = 'epguide',
-    version = '1.8.0-Alpha2',
+    version = '1.8.0',
     packages = find_packages(exclude=['bin','test']),
     package_data = {'': ['AUTHORS', 'LICENSE',
     'CHANGES', 'README', 'epguide_run']},
