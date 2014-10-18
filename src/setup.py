46c26593f37f06008d98967c8c7ce0c9d4109e64
S?awek Miku?a <slawek.mikula@gmail.com>
2014-10-18 10:38:20 +0200

    - wersja 1.9.1

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup (
     name = 'epguide',
-    version = '1.9.0',
+    version = '1.9.1',
     packages = find_packages(exclude=['bin','test']),
     package_data = {'': ['AUTHORS', 'LICENSE',
     'CHANGES', 'README', 'epguide_run']},
